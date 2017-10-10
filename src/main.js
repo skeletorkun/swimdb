741744a5254e5c3b180ad7c17ba53012d748ce97
Orkun OZEN (contractor) <oozen@amadeus.com>
2017-10-10 10:17:40 +0200

    add react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-Console.log('Hello world');
+console.log('Hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
