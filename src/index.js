b2ca52f258cda6ffd5801bb5a375f6d253fde264
Orkun Ozen <skeletorkun@gmail.com>
2020-02-08 23:15:43 +0100

    npm update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import Main from './components/Main'
 import LoginComponent from './components/user/LoginContainer'
@@ -13,11 +12,6 @@ import registerServiceWorker from './registerServiceWorker'
 import withTracker from './analytics'
 
 import './index.css'
-
-// Material UI
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
 const store = configureStore();
 
