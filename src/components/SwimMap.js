933ff6ae2cdd54a92c280371bfa4edbbafb1dbe2
Orkun Ozen <skeletorkun@gmail.com>
2020-04-29 19:31:49 +0300

    Update SwimMap.js

--- src/components/SwimMap.js
+++ src/components/SwimMap.js
@@ -8,7 +8,7 @@ import InfoWindow from "./map/InfoWindow"
 
 const Map = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAbsXVAovwScQcJ4wHfBol0nJTizRjKX18&v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=[keyHere]&v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{height: `100%`}}/>,
         containerElement: <div style={{height: `100%`}}/>,
         mapElement: <div style={{height: `100%`}}/>,
